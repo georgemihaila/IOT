ce983a6102537363f8ed73a02cff95a5addbf23d
georgemihaila <george_mihaila@outlook.com>
2018-06-02 11:57:49 +0300

    Fixed untracked files

--- ESP8266 RGBWW Light Control/ESP8266PWMControlServer/Debug/core/cencode.c.d
+++
@@ -1,3 +0,0 @@
-C:\Users\George\AppData\Local\Temp\VMBuilds\ESP8266PWMControlServer\esp8266_modwifi\Debug\core\cencode.c.o: \
- C:\Users\George\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266\libb64\cencode.c \
- C:\Users\George\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266\libb64\cencode.h
--- ESP8266 RGBWW Light Control/ESP8266PWMControlServer/Debug/core/cont.S.d
+++
@@ -1,2 +0,0 @@
-C:\Users\George\AppData\Local\Temp\VMBuilds\ESP8266PWMControlServer\esp8266_modwifi\Debug\core\cont.S.o: \
- C:\Users\George\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266\cont.S
